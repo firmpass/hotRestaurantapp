cdcc1d666229749701b6768b33fcfd0d6169d138
skibbidyska <dustin.wright3@gmail.com>
2018-05-05 10:46:40 -0400

    added tableArray rrequirement to server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-//require express, body parser, and path
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require ('path');
-
+var tableArray = requre ('./tableArray');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
